c666974e40dcf53c2da62f536f8ee09ecacf5934
SofianeLasri <alasri250@gmail.com>
2021-04-19 16:44:17 +0200

    Update vbcms-config.php

--- vbcms-config.php
+++ vbcms-config.php
@@ -15,7 +15,7 @@ DATABASE CONFIGURATION
 $bddHost = ''; //Adresse du serveur MySQL
 $bddUser = ''; //Utilisateur
 $bddMdp = ''; //Mot de passe
-$bddName = ''; //Nom de la base de donnée
+$bddName = ''; //Nom de la base de données
 
 //error_reporting(0); //Désactive les erreurs
 ?>
